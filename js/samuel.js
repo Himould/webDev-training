04bee7c9e3b5a8e0a7af4b0e31c321bd5a9e50c3
samuel <ogundeleomoniyi@gmail.com>
2024-08-29 21:48:43 +0100

    slice

--- js/samuel.js
+++ js/samuel.js
@@ -2,6 +2,16 @@
 
 // console.log(myName.length)
 
-var tweet = prompt("type your tweet here");
+
+// LENGTH
+// var tweet = prompt("type your tweet here");
 // var tweetCount = tweet.length;
-console.log("you have written " + tweet.length + " characters, you have " + (200 - tweet.length) + " characters left");
+// console.log("you have written " + tweet.length + " characters, you have " + (200 - tweet.length) + " characters left");
+
+// SLICE PROGRAMMERS ALWAYS COUNT FROM ZERO
+// var school = "obafemi";
+// console.log(school.slice(0,2));
+
+tweet.slice(0,5);
+
+console.log(prompt("type your tweet here").slice(0,5));
