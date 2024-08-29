a71791520cb994ea4f44ca6e50cd528c41b1d1c8
samuel <ogundeleomoniyi@gmail.com>
2024-08-29 21:13:56 +0100

    js-promt,length

---
+++ js/samuel.js
@@ -0,0 +1,7 @@
+// var myName = "niyi";
+
+// console.log(myName.length)
+
+var tweet = prompt("type your tweet here");
+// var tweetCount = tweet.length;
+console.log("you have written " + tweet.length + " characters, you have " + (200 - tweet.length) + " characters left");
