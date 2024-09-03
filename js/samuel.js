4a60f8a1906f389c5980d010206f78ba5b4d45ce
samuel <ogundeleomoniyi@gmail.com>
2024-09-03 17:59:46 +0100

    if staement

--- js/samuel.js
+++ js/samuel.js
@@ -113,3 +113,48 @@ function isEven(num){
    console.log("even")}
 }
 isEven(num);
+
+const phrase = 'slow';
+function getColor(phrase){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if (phrase === 'stop'){
+       console.log('red');
+   } else if(phrase === 'slow'){
+       console.log('yellow');
+   } else if(phrase === 'go'){
+       console.log('green');
+   } else {
+       console.log('purple');
+   }
+ 
+   
+   
+    //AND THIS LINE ↑↑↑↑↑
+}
+
+/*// Nesting
+// Change the value of num, so that "YOU GOT ME!" prints out
+let num = 0; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :) 
+
+// DO NOT TOUCH ANYTHING BELOW (please) 
+if(num <= 100) {
+    if(num >= 50) {
+        console.log("HEY!");
+    }
+} else {
+    if (num < 103) {
+        if(num % 2 === 0){
+            console.log("YOU GOT ME!");
+        }
+    }
+} */
+
+
+    const mystery = ''; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+
+
+// LEAVE THIS CODE ALONE! (pretty please)
+if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+    console.log("YOU GOT IT!!!");
+}
