89aed342b9d5912a7cbc7184bbfcd2f48a5217ac
samuel <ogundeleomoniyi@gmail.com>
2024-09-15 15:02:05 +0100

    INTRO TO DOM PROPER

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,7 @@
+document.query
+document.getElementsByTagName('li')[2].innerHTML = 'omoniyi';
+'omoniyi'
+
+document.querySelectorAll('li')[2].innerText = 'omoniyi';
+
+document.querySelectorAll('li')[2].style.color = 'green';
