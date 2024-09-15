fa74c4ed996aef42beb519e314930620b2463907
samuel <ogundeleomoniyi@gmail.com>
2024-09-15 21:53:51 +0100

    manipulation html,class,attributes,text

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -5,3 +5,13 @@ document.getElementsByTagName('li')[2].innerHTML = 'omoniyi';
 document.querySelectorAll('li')[2].innerText = 'omoniyi';
 
 document.querySelectorAll('li')[2].style.color = 'green';
+
+document.querySelector('li a').style.color = 'red'
+
+document.querySelector('button').style.backgroundColor = "yellow";
+
+document.querySelector('h1').classList.add('huge');
+
+document.querySelector('a').getAttribute('href')
+
+document.querySelector('a').setAttribute('href', 'https://www.fb.com');
