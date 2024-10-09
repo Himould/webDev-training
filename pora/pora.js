a88f96deb66b0ac1a4ff6e7b26b1b9f75477ca67
samuel <ogundeleomoniyi@gmail.com>
2024-10-09 14:24:07 +0100

    node -pora

--- pora/pora.js
+++ pora/pora.js
@@ -2,5 +2,6 @@ let myName = "Ogundele Omoniyi"
 let msg;
 msg = 'hello';
 msg = true;
+let header = '238554'
 msg = {name: 'niyi', age: 2};
-console.log(msg, typeof msg);
+console.log(msg, typeof header, Number(header));
