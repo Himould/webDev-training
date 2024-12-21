12de5225232c546e63284f8f3ce9ec3a7fd5a21f
Himould <ogundeleomoniyi@gmail.com>
2024-12-21 16:04:43 +0100

    CRUD AND MIDDLEWARE...route and app level

--- BACKEND/express crash course/routes/post.js
+++ BACKEND/express crash course/routes/post.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 const router = express.Router();
 
 
@@ -34,6 +34,13 @@ let posts = [
     {id:3, title: 'post three'}
 ]
 
+// LOGGER MIDDLEWARE ROUTE LEVEL e.g on get route line 66
+
+// const logger = (req, res, next) => {
+//     console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+//     next();
+// };
+
 // GET ALL POST
 
 // router.get("/",  (req, res) => {
@@ -54,15 +61,15 @@ router.get("/:id",  (req, res) => {
     // res.json(posts.filter((post) => post.id === id));
 })
 
-// QUERRY STRING
+// QUERRY STRING, with logger middleware on route level
 
-router.get("/",  (req, res) => {
-    const limit = parseInt(req.query.limit);
-    if(!isNaN(limit) && limit > 0){
-     return res.json(posts.slice(0, limit))
-    }
-    res.json(posts);
-});
+// router.get("/", logger, (req, res) => {
+//     const limit = parseInt(req.query.limit);
+//     if(!isNaN(limit) && limit > 0){
+//      return res.json(posts.slice(0, limit))
+//     }
+//     res.json(posts);
+// });
 
 // STATUS CODE
 
@@ -73,5 +80,47 @@ router.get("/",  (req, res) => {
     }
     res.status(200).json(posts);
 });
+
+// CREATE NEW POST
+
+router.post('/', (req, res) => {
+    const newPost = {
+        id: posts.length +1,
+        title: req.body.title
+    }
+    if(!newPost.title){
+        return res.status(400).json({msg: 'please include a title'})
+    }
+    posts.push(newPost)
+    res.status(201).json(posts);
+})
+
+// UPDATE POST USING PUT
+
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post) {
+        return res.status(404).json({msg:`a post of ${id} not found`})
+    }
+    post.title = req.body.title;
+    res.status(200).json(posts);
+})
+
+// DELETE POST
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post) {
+        return res.status(404).json({msg:`a post of ${id} not found`})
+    }
+    posts = posts.filter((post) => post.id !== id)
+    res.status(200).json(posts);
+})
+
+
+
 
 export default router;
--- BACKEND/express crash course/server.js
+++ BACKEND/express crash course/server.js
@@ -1,13 +1,22 @@
 import express from "express";
 import posts from './routes/post.js'
+import logger from './middleware/logger.js'
 // import path from 'path'
 // import { dirname } from 'path';
 // import { fileURLToPath } from 'url';
 // const  __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express();
-
 const port = 8080;
+
+// BODY PARSER MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+// LOGGER MIDDLEWARE
+
+app.use(logger);
+
 
 // ROUTES
 app.use('/api/posts', posts);
